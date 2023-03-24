e1aa7e3450508db3089e4abc83eeed882330a018
Reeya08 <reeyanadeem3@gmail.com>
2023-03-24 22:23:46 +0500

    initial commit

--- lib/presentation/views/register_contacts/register_contacts_view.dart
+++ lib/presentation/views/register_contacts/register_contacts_view.dart
@@ -10,41 +10,43 @@ class RegisterContactView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppConstants.whiteBackgroundColor,
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Image.asset(
-            'assets/images/register-contact.png',
-            height: 300,
-            width: 300,
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          const CustomTextField(
-            ImagePath: 'assets/images/name.png',
-            LabelText: 'Name',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const CustomTextField(
-            ImagePath: 'assets/images/phone.png',
-            LabelText: 'Phone',
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          CustomButton(
-            childText: 'Add Contact',
-            height: 60,
-            width: 230,
-            textSize: 20,
-            onPressed: () {},
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Image.asset(
+              'assets/images/register-contact.png',
+              height: 300,
+              width: 300,
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            const CustomTextField(
+              ImagePath: 'assets/images/name.png',
+              LabelText: 'Name',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const CustomTextField(
+              ImagePath: 'assets/images/phone.png',
+              LabelText: 'Phone',
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            CustomButton(
+              childText: 'Add Contact',
+              height: 60,
+              width: 230,
+              textSize: 20,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
